5c332082edb2cd2645e1c9dda99eedaa1f9c4a83
Matheus Gomes <matheusgomes062@gmail.com>
2022-03-11 15:16:14 -0300

    fix: vite is client

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { isClient } from '@vueuse/core'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -14,7 +15,9 @@ export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+    if (isClient) {
+      // install all modules under `modules/`
+      Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+    }
   },
 )
