97f3f3ee606c148d562d70be7796f4e246f4b2b8
Matheus Gomes <47605309+matheusgomes062@users.noreply.github.com>
2022-03-15 21:37:42 -0300

    Update Title.vue

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -7,15 +7,10 @@ h1(class="text-6xl font-900 flex justify-center my-10") Cv Online
 
 <style lang="scss" scoped>
 h1 {
-background-image: linear-gradient(
-  90deg,
-  hsl(333deg 93% 56%) 0%,
-  hsl(314deg 72% 66%) 15%,
-  hsl(270deg 80% 79%) 40%,
-  hsl(227deg 100% 82%) 66%,
-  hsl(206deg 100% 72%) 87%,
-  hsl(194deg 85% 62%) 100%
-);
+  background: #03001e;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #fdeff9, #ec38bc, #7303c0, #03001e);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #fdeff9, #ec38bc, #7303c0, #03001e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
